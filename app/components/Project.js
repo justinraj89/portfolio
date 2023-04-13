d0e9f0fc89adbae69376ec45a6490bb6e85b962b
Justin Rajewski <rajewskijustin@gmail.com>
2023-04-13 11:02:41 -0700

    refactored project page so that projects are not hard coded. Data being pulled from ProjectData file now

---
+++ app/components/Project.js
@@ -0,0 +1,67 @@
+import { SiMongodb } from "react-icons/si";
+import { SiExpress } from "react-icons/si";
+import { SiReact } from "react-icons/si";
+import { SiNodedotjs } from "react-icons/si";
+import { SiHtml5 } from "react-icons/si";
+import { SiNextdotjs } from "react-icons/si";
+import { SiTailwindcss } from "react-icons/si";
+import { SiStyledcomponents } from "react-icons/si";
+import { SiCsswizardry } from "react-icons/si";
+import { SiJavascript } from "react-icons/si";
+import { SiPython } from "react-icons/si";
+import { SiDjango } from "react-icons/si";
+import { SiPostgresql } from "react-icons/si";
+//=======================================================
+
+function Project({
+  projectName,
+  logo,
+  description,
+  gitHubLink,
+  deployLink,
+  technologiesUsed,
+}) {
+  const technologyLogos = {
+    MongoDB: <SiMongodb className="h-8 w-8 text-green-300" />,
+    Express: <SiExpress className="h-8 w-8 text-blue-300" />,
+    React: <SiReact className="h-8 w-8 text-sky-400" />,
+    Node: <SiNodedotjs className="h-8 w-8 text-green-600" />,
+    Next: <SiNextdotjs className="h-8 w-8 text-gray-100" />,
+    Tailwind: <SiTailwindcss className="h-8 w-8 text-teal-400" />,
+    HTML: <SiHtml5 className="h-8 w-8 text-orange-600" />,
+    StyledComponents: <SiStyledcomponents className="h-9 w-9 text-pink-400" />,
+    Javascript: <SiJavascript className="h-8 w-8 text-yellow-300" />,
+    CSS: <SiCsswizardry className="h-8 w-8 text-blue-600" />,
+    Python: <SiPython className="h-9 w-9 text-yellow-400" />,
+    Django: <SiDjango className="h-8 w-8 text-emerald-800" />,
+    PostgreSQL: <SiPostgresql className="h-8 w-8 text-sky-500" />,
+  };
+
+  return (
+    <article className="text-gray-300 shadow-xl drop-shadow-lg p-6 w-80 rounded-lg mb-6 lg:transition lg:ease-in-out lg:delay-100 lg:hover:scale-[1.02] lg:duration-300">
+      <a target="_blank" rel="noopener noreferrer" href={deployLink}>
+      <h2 className="text-3xl font-bold text-center font-spaceMono pb-2">
+        {projectName}
+      </h2>
+      <div className="flex justify-center">
+        <img src={logo} alt={projectName} className="w-24" />
+      </div>
+
+      <p className=" pt-4 font-spaceMono text-center">{description}</p>
+
+      <div className="flex justify-evenly pt-4 pb-4">
+        {technologiesUsed.map((technology) => technologyLogos[technology])}
+      </div>
+
+      <a target="_blank" rel="noopener noreferrer" href={gitHubLink}>
+        <div className="text-lg font-spaceMono text-blue-300 hover:text-blue-400 font-semibold text-center">
+          Github Repo
+        </div>
+      </a>
+      </a>
+    </article>
+
+  );
+}
+
+export default Project;
---
+++ app/utils/ProjectData.js
@@ -0,0 +1,50 @@
+export const ProjectData = [
+    {
+        projectName: 'Moovie',
+        logo: 'https://i.imgur.com/Zgq8CEM.png',
+        description: 'Moovie is a MERN stack application that allows users to create an account, login, and build their own custom watchlists. All movie data is obtained from TMDB API. JWTs used for authentication.',
+        gitHubLink: 'https://github.com/justinraj89/Moovie',
+        deployLink: 'https://m00vieapp.herokuapp.com/',
+        technologiesUsed: ['MongoDB', 'Express', 'React', 'Node']
+    },
+    {
+        projectName: 'Dictionary',
+        logo: 'https://i.imgur.com/t5OwXJA.png',
+        description: 'Dictionary application built using Nextjs and Tailwind CSS. Word data is provided through 3rd party API (dictionaryapi.dev). Darkmode feature implemented using Next/Themes.',        
+        gitHubLink: 'https://github.com/justinraj89/dictionary-next13',
+        deployLink: 'https://dictionary-nextjs.herokuapp.com/',
+        technologiesUsed: ['Next', 'React', 'Tailwind']
+    },
+    {
+        projectName: 'Doodle-Gram',
+        logo: 'https://i.imgur.com/aadtTbA.png',
+        description: 'Doodle-Gram is an Instagram-type full stack CRUD application. Users can login using their Google account, browse a collection of posted  artwork, add their own artwork, comment, edit and delete posts.',     
+        gitHubLink: 'https://github.com/justinraj89/Doodle-Gram',
+        deployLink: 'https://doodle-gram.herokuapp.com/artwork',
+        technologiesUsed: ['Node', 'Express', 'HTML', 'MongoDB']
+    },
+    {
+        projectName: 'React Music Player',
+        logo: 'https://i.imgur.com/R0rDbTC.png',
+        description: 'Fully Responsive music player built using React. In this project I experimented with the Styled Components package, and learned more  about using Audio/mp3 files in an application. I enjoy making music, and wanted a place to put some of my music',              
+        gitHubLink: 'https://github.com/justinraj89/react-music-player',
+        deployLink: 'https://music-player-reactjs.herokuapp.com/',
+        technologiesUsed: ['StyledComponents', 'React', 'HTML']
+    },
+    {
+        projectName: 'War',
+        logo: 'https://i.imgur.com/kHLrqgD.png',
+        description: 'This was my first project at General Assemblys SEI program. The Goal was to build a browser game of our choice. I decided to go with the classic card game "War". Application was built with vanilla Javascript, HTML, and CSS.',                      
+        gitHubLink: 'https://github.com/justinraj89/War',
+        deployLink: 'https://glittery-souffle-314a7c.netlify.app/',
+        technologiesUsed: ['Javascript', 'HTML', 'CSS']
+    },
+    {
+        projectName: 'Beastcoast Memes',
+        logo: 'https://i.imgur.com/cHFY8Eh.png',
+        description: 'This was a group project I worked on at General Assemblys SEI program. Our cohort was broken up into groups of 4 and were tasked with creating a full stack application using Python, Django, and PostgreSQL. My roles on This project were "Database Manager" and "Lead Designer". This was a great learning experience working with a team and using GIT.',                          
+        gitHubLink: 'https://github.com/justinraj89/Beastcoast-Memes',
+        deployLink: 'https://beastcoastmeme.herokuapp.com/',
+        technologiesUsed: ['Python', 'Django', 'PostgreSQL']
+    },
+]
